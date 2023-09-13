9d8edbcfea3f764c18d040bef8e738b5f3c374cf
Kamol-Paul <s1910176121@ru.ac.bd>
2023-09-13 14:47:25 +0600

    cors finnaly succeed

--- src/main/java/com/chobichokro/ChobiChokroApi.java
+++ src/main/java/com/chobichokro/ChobiChokroApi.java
@@ -2,7 +2,9 @@ package com.chobichokro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class ChobiChokroApi {
--- src/main/java/com/chobichokro/security/WebSecurityConfig.java
+++ src/main/java/com/chobichokro/security/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.chobichokro.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +19,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import com.chobichokro.security.jwt.AuthEntryPointJwt;
 import com.chobichokro.security.jwt.AuthTokenFilter;
 import com.chobichokro.security.services.UserDetailsServiceImpl;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -62,12 +68,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     return new AuthTokenFilter();
   }
 
-//@Override
-//public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
-//  authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-//}
-
-  @Bean
+    @Bean
   public DaoAuthenticationProvider authenticationProvider() {
     DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 
@@ -77,13 +78,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     return authProvider;
   }
 
-//@Bean
-//@Override
-//public AuthenticationManager authenticationManagerBean() throws Exception {
-//  return super.authenticationManagerBean();
-//}
-
-  @Bean
+    @Bean
   public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
     return authConfig.getAuthenticationManager();
   }
@@ -93,33 +88,37 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     return new BCryptPasswordEncoder();
   }
 
-//@Override
-//protected void configure(HttpSecurity http) throws Exception {
-//  http.cors().and().csrf().disable()
-//    .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-//    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-//    .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-//    .antMatchers("/api/test/**").permitAll()
-//    .anyRequest().authenticated();
-//
-//  http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//}
-
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.csrf(AbstractHttpConfigurer::disable)
         .exceptionHandling(exception -> exception.authenticationEntryPoint(this.unauthorizedHandler))
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
-                auth -> auth.requestMatchers(PUBLIC_URLS).permitAll()
-                        .requestMatchers(PRIVATE_URLS).authenticated()
-                .anyRequest()
-                .permitAll());
+                auth -> {
+                    auth.requestMatchers(PUBLIC_URLS).permitAll()
+                            .requestMatchers(PRIVATE_URLS).authenticated()
+                            .anyRequest()
+                            .permitAll();
+                });
 
     http.authenticationProvider(authenticationProvider());
 
     http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
     return http.build();
   }
+
+  private CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOriginPatterns(List.of("*"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Access-Control-Allow-Origin"));
+    configuration.setAllowCredentials(true);
+    configuration.setExposedHeaders(List.of("Authorization"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
 }
