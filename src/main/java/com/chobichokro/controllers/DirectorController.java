646185f9bec396d104ba97b2271cf0ab718c347f
Kamol-Paul <s1910176121@ru.ac.bd>
2023-09-18 15:50:40 +0600

    single movie analysis added

--- src/main/java/com/chobichokro/controllers/DirectorController.java
+++ src/main/java/com/chobichokro/controllers/DirectorController.java
@@ -3,10 +3,7 @@ package com.chobichokro.controllers;
 import com.chobichokro.controllerHelper.DirectorHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/director")
@@ -16,5 +13,13 @@ public class DirectorController {
     @GetMapping("/get/analysis")
     public ResponseEntity<?> getAnalysis(@RequestHeader("Authorization") String token){
         return directorHelper.getDirectorAnalysis(token);
+    }
+    @GetMapping("/get/analysis/{movieId}")
+    public ResponseEntity<?> getSingleMovieAanlysis(@RequestHeader("Authorization") String token, @PathVariable String movieId){
+        return directorHelper.getSingleMovieAnalysis(token, movieId);
+    }
+    @GetMapping("/get/myMovies")
+    public ResponseEntity<?> getMyMovies(@RequestHeader("Authorization") String token){
+        return directorHelper.getMyMovies(token);
     }
 }
