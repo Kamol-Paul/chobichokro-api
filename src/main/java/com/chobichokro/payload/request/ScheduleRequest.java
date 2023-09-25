13b69b42cd449c1840316171afa66e5d2fb435b4
Kamol-Paul <s1910176121@ru.ac.bd>
2023-09-26 01:38:15 +0600

    upcoming new movie list

--- src/main/java/com/chobichokro/payload/request/ScheduleRequest.java
+++ src/main/java/com/chobichokro/payload/request/ScheduleRequest.java
@@ -3,10 +3,12 @@ package com.chobichokro.payload.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 public class ScheduleRequest {
     String movieName;
     String theaterId;
